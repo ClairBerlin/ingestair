ad1f275a88199714b8ccd53fb2bc798afd43db37
Ulrich Schuster <ulrich.schuster@koing.de>
2021-05-13 23:14:43 +0200

    Configure RIO logging subsystem

--- ingestair/Configuration.hs
+++ ingestair/Configuration.hs
@@ -5,6 +5,7 @@
 module Configuration
   ( EnvironmentSettings(..)
   , defaultEnvironmentSettings
+  , configureLogging
   , HasEnvironment
   , environmentL
   , Env(..)
@@ -12,6 +13,7 @@ module Configuration
 where
 
 import           RIO
+import qualified RIO.Text                      as T
 
 import qualified Network.Wai.Handler.Warp      as Warp
 import qualified System.Envy                   as EV
@@ -23,6 +25,11 @@ import qualified Ingestair.Persistence.Api     as DB
 -- Runtime system environment variables used for configuring the application.
 data EnvironmentSettings = EnvironmentSettings
   { serverPort :: !Warp.Port -- SERVER_PORT
+  , logLevel :: !Text -- LOG_LEVEL
+  , logVerbose :: Bool -- LOG_VERBOSE
+  , logUseTime :: Bool -- LOG_USE_TIME
+  , logUseLoc :: Bool -- LOG_USE_LOC
+  , logUseColor :: Bool -- LOG_USE_COLOR
   , sqlHost :: !Text -- SQL_HOST
   , sqlPort :: !Word16 -- SQL_PORT
   , sqlDatabase :: !Text -- SQL_DATABASE
@@ -33,12 +40,36 @@ data EnvironmentSettings = EnvironmentSettings
 defaultEnvironmentSettings :: EnvironmentSettings
 defaultEnvironmentSettings = EnvironmentSettings
   { serverPort  = 8081
+  , logLevel    = "WARN"
+  , logVerbose  = False
+  , logUseTime  = True
+  , logUseLoc   = False
+  , logUseColor = True
   , sqlHost     = "localhost"
   , sqlPort     = 5432
   , sqlDatabase = "managairdb_dev"
   , sqlUser     = "managair_dev"
   , sqlPassword = "postgres"
   }
+
+readLogLevel :: Text -> Maybe LogLevel
+readLogLevel lvl = case (T.toUpper . T.strip) lvl of
+  "DEBUG" -> Just LevelDebug
+  "INFO"  -> Just LevelInfo
+  "WARN"  -> Just LevelWarn
+  "ERROR" -> Just LevelError
+  _       -> Nothing
+
+
+configureLogging :: EnvironmentSettings -> LogOptions -> LogOptions
+configureLogging eEnv logOptions' =
+  let maybeLogLevel = readLogLevel (logLevel eEnv)
+      level         = fromMaybe LevelWarn maybeLogLevel
+  in  setLogMinLevel level
+        $ setLogVerboseFormat (logVerbose eEnv)
+        $ setLogUseTime (logUseTime eEnv)
+        $ setLogUseLoc (logUseLoc eEnv)
+        $ setLogUseColor (logUseColor eEnv) logOptions'
 
 -- Instance for automatically retrieving environment variables.
 instance EV.FromEnv EnvironmentSettings
--- ingestair/Main.hs
+++ ingestair/Main.hs
@@ -12,27 +12,30 @@ import qualified System.Envy                   as EV
 
 import qualified Ingestair.Controller.Server   as S
 import qualified Ingestair.Persistence.Api     as DB
-import qualified Logging                       as L
 import qualified Configuration                 as Conf
 
 
 main :: IO ()
 main = do
   -- Read configuration from environment variables.
-  eEnv <- EV.decodeWithDefaults Conf.defaultEnvironmentSettings
+  eEnv        <- EV.decodeWithDefaults Conf.defaultEnvironmentSettings
+  -- Route logging to stdout
+  logOptions' <- logOptionsHandle stdout True
   -- Set up the application configuration that is threaded throughout the entire 
   -- application using the ReaderT design pattern by means of the RIO monad.
-  let dBConfig = DB.initDb (Conf.sqlHost eEnv)
-                           (Conf.sqlPort eEnv)
-                           (Conf.sqlDatabase eEnv)
-                           (Conf.sqlUser eEnv)
-                           (Conf.sqlPassword eEnv)
-      env = Conf.Env { Conf.logger      = L.myLogFunc
-                     , Conf.environment = eEnv
-                     , Conf.dBConfig    = dBConfig
-                     }
+  let logOptions = Conf.configureLogging eEnv logOptions'
+      dBConfig   = DB.initDb (Conf.sqlHost eEnv)
+                             (Conf.sqlPort eEnv)
+                             (Conf.sqlDatabase eEnv)
+                             (Conf.sqlUser eEnv)
+                             (Conf.sqlPassword eEnv)
       serverPort = Conf.serverPort eEnv
-  -- Apply middleware and start the web server:
-  -- > Support for gzip compression.
-  -- > Log requests to stdout.
-  Warp.run serverPort (GZ.gzip GZ.def (RL.logStdout $ S.serverApp env))
+  withLogFunc logOptions $ \logFunc -> do
+    let env = Conf.Env { Conf.logger      = logFunc -- L.myLogFunc
+                       , Conf.environment = eEnv
+                       , Conf.dBConfig    = dBConfig
+                       }
+    -- Apply middleware and start the web server:
+    -- > Support for gzip compression.
+    -- > Log requests to stdout.
+    Warp.run serverPort (GZ.gzip GZ.def (RL.logStdout $ S.serverApp env))
